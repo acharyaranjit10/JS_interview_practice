d7a778a5e5387a235669beff37a562b0ff99be06
acharyaranjit10 <159681278+acharyaranjit10@users.noreply.github.com>
2024-06-26 12:06:28 +0545

    git remote add origin https://github.com/acharyaranjit10/acharyaranjit10.git

---
+++ magicalSortingHat.js
@@ -0,0 +1,21 @@
+// The Magical Sorting Hat: Imagine you are creating a magical sorting hat for a wizard school. Implement a JavaScript function that takes an array of student names and assigns them to one of the four houses (Gryffindor (length less than 6), Hufflepuff(length less than 8), Ravenclaw(length less than 12), or Slytherin(length greater than or equal to 12)) based on the length of their names.
+
+let names = ["Ram", "Shyam", "Ranjit", "Ramkrishna", "VenuGopalIyer", "ChennaSwamiMuttuSwami", "Adhibesh", "Shlok"];
+let houses = [];
+
+for (const name of names) {
+    if (name.length < 6) {
+        houses.push("Gryffindor");
+    }
+    else if(name.length < 8){
+        houses.push("Hufflepuff");
+    }
+    else if(name.length < 12){
+        houses.push("Ravenclaw");
+    }
+   else {
+        houses.push("Slytherin");
+    };
+};
+
+console.log(houses);
